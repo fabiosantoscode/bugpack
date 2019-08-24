53dafffb155bd7601808033ed67ffe13a9a6f9fa
Fábio Santos <fabiosantosart@gmail.com>
2019-08-24 12:12:27 +0100

    simplify repo

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-import { test } from './bug'
-
-test().then(console.log)
+// Import your things here
+// Then try to reproduce your bug in this file
+// If you need to simulate exporting something, use console.log to make sure Terser doesn't just remove the problem code and makes everything pass.
+// Don't export stuff from here, if you need to do this create a separate file and export stuff from there.
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,16 @@ const path = require("path")
 
 module.exports = {
     mode: "production",
-    entry: "./bug.js",
+    entry: "./main.js",
     optimization: {
         minimizer: [
             new (require("terser-webpack-plugin"))({
-                parallel: false,
-                terserOptions: {
-                    ecma: 8,
-                    keep_classnames: true,
-                    keep_fnames: true,
-                },
+                parallel: false  // Work around a bug in terser-webpack-plugin that shows bad stack traces.
             })
         ]
     },
     output: {
         path: path.resolve(__dirname, "build"),
-        filename: "bug.out.js"
+        filename: "main.out.js"
     }
-}
+};
